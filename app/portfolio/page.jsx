9883846ebeaaebb214e76932bad6789f432fefd9
Muhammad-Naveed704 <amirprinceak5@gmail.com>
2024-05-14 00:18:09 +0500

    portfolio page

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,14 +1,80 @@
-"use client"
+"use client";
+import { motion, useScroll, useTransform } from "framer-motion";
+// import Image from "next/image";
+import Image from 'next/image'
+import Link from "next/link";
+import { useRef } from "react";
 
-import React from 'react'
-import {motion} from "framer-motion";
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    link: "https://lama.dev",
+  },
+];
 
 const PortfolioPage = () => {
-  return (
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
-  about
-    </motion.div>
-  )
-}
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
 
-export default PortfolioPage
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-[600vh] relative" ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">My Works</div>
+     <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden ">
+      <motion.div style={{x}} className="flex">
+
+      {items.map((item) => (
+        <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id}>
+          <div className="flex flex-col gap-8 text-white">
+            <h1> {item.title}</h1>
+            <div className="relative">
+            {/* <Image src={item.img} alt='projects image' width={1260} height={750} fill /> */}
+            <Image src={item.img} alt='projects image' fill /> 
+            </div>
+            <p>{item.desc}</p>
+            <Link href={item.link}><button>See Demo</button></Link>
+          </div>
+        </div>
+      ))}
+          </motion.div>
+     </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default PortfolioPage;
